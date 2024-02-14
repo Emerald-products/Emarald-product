2177a3a17e449bc4917fa3e476375f9ac903d3fb
Barakat Bolaji <barakatbolaji65@gmail.com>
2024-02-14 09:40:57 +0100

    - Installing react router dom - creating path - creating components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import LogIn from "./Pages/LogIn";
 
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
